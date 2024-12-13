9d1725c588e0ef56f4984da8cc4397db1d87c6e3
mxyr <mxyr007@126.com>
2024-12-13 18:36:16 +0800

    更新

--- adb模式函数.py
+++ adb模式函数.py
@@ -66,7 +66,9 @@ class 模拟器实例:
         图片保存路径 = self.截图()
         原始图片 = self.图片信息读取(图片保存路径)
         匹配图片信息 = self.图片信息读取(匹配图片)
-        匹配结果 = pyscreeze.locate(匹配图片信息, 原始图片, confidence=self.匹配可信度)
+        匹配结果 = pyscreeze.locate(
+            匹配图片信息, 原始图片, confidence=self.匹配可信度, grayscale=False
+        )
         中心坐标 = pyscreeze.center(匹配结果)
         if self.debug:
             self.打印并写入日志(匹配图片 + "中心坐标为：" + str(中心坐标))
@@ -94,7 +96,27 @@ class 模拟器实例:
         self.打印并写入日志(f"点击{x},{y}")
         time.sleep(self.操作延迟)
 
-    def 点击按钮(self, 图标名称):
+    def 等待_直到找到(self, 图片名称: str, 超时: int):
+        图片 = os.path.join(self.图标路径, f"{图片名称}.png")
+        起始时间 = time.time()
+        耗时 = 0
+        while 耗时 < 超时:
+            耗时 = time.time() - 起始时间
+            try:
+                self.打印并写入日志(f"尝试寻找：{图片名称}")
+                x坐标, y坐标 = self.图片定位(图片)
+                break
+            except pyscreeze.ImageNotFoundException:
+                self.打印并写入日志(f"未找到图片！正在重试。耗时：{耗时}；超时：{超时}")
+                time.sleep(self.重试延迟)
+        if 耗时 < 超时:
+            self.打印并写入日志(f"找到：{图片名称}；坐标：{x坐标}，{y坐标}")
+            return x坐标, y坐标
+        else:
+            self.打印并写入日志(f"找不到图片（{图片名称}）！超时：{超时}")
+            return -1
+
+    def 点击按钮(self, 图标名称, dx=0, dy=0, 次数=1):
         图标 = os.path.join(self.图标路径, f"{图标名称}.png")
         尝试次数 = 0
         while 尝试次数 < self.重试次数:
@@ -109,7 +131,9 @@ class 模拟器实例:
                 )
                 time.sleep(self.重试延迟)
         if 尝试次数 < self.重试次数:
-            self.点击(x坐标, y坐标)
+            while 次数 > 0:
+                self.点击(x坐标 + dx, y坐标 + dy)
+                次数 -= 1
         else:
             self.打印并写入日志(f"失败，找不到图片（{图标名称}）！程序结束！")
             if self.log:
@@ -135,7 +159,8 @@ class 模拟器实例:
                         "等待确认" + str(计数器) + "/" + str(self.加载判定次数)
                     )
 
-    def 尝试寻找(self, 图片名称):
+    def 尝试寻找(self, 图片名称: str):
+        # 输入图片名称，使用默认图标路径，若找到返回坐标值，若找不到返回-1
         图片 = os.path.join(self.图标路径, f"{图片名称}.png")
         尝试次数 = 0
         while 尝试次数 < self.重试次数:
@@ -150,17 +175,38 @@ class 模拟器实例:
                 )
                 time.sleep(self.重试延迟)
         if 尝试次数 < self.重试次数:
+            self.打印并写入日志(f"找到：{图片名称}；坐标：{x坐标}，{y坐标}")
             return x坐标, y坐标
         else:
             self.打印并写入日志(f"找不到图片（{图片名称}）！")
-            if self.log:
-                self.日志关闭()
             return -1
+
+    def 尝试寻找并点击(self, 图片名称):
+        查找结果 = self.尝试寻找(图片名称)
+        if 查找结果 != -1:
+            self.点击(查找结果[0], 查找结果[1])
+
+    def 等待_直到找到并点击(self, 图片名称: str, 超时: int):
+        查找结果 = self.等待_直到找到(图片名称, 超时)
+        if 查找结果 != -1:
+            self.点击(查找结果[0], 查找结果[1])
+
+    def 点击_直到消失(self, 图片名称):
+        找到 = True
+        while 找到:
+            查找结果 = self.尝试寻找(图片名称)
+            if 查找结果 == -1:
+                找到 = False
+            else:
+                self.点击(查找结果[0], 查找结果[1])
 
     def 屏幕分辨率(self):
         指令 = f"{self.adb指令头} shell wm size"
         返回值 = subprocess.Popen(指令, shell=True, stdout=subprocess.PIPE).stdout
-        return 返回值
+        返回值 = 返回值.read().decode("gbk")
+        分辨率文字 = 返回值.split(":")[1][1:-2]
+        分辨率 = 分辨率文字.split("x")
+        return int(分辨率[0]), int(分辨率[1])
 
     def 返回键(self):
         指令 = f"{self.adb指令头} shell input keyevent 4"
